ffbe07581c5f9a1593578e3b00b19814ee063663
Ubuntu <ubuntu@ip-172-31-20-56.us-west-2.compute.internal>
2013-07-10 13:05:20 +0000

    async read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var filename = 'index.html';
+  var buf = new Buffer(fs.readFileSync(filename));
+
+  response.send(buf.toString('utf8');
 });
 
 var port = process.env.PORT || 5000;
